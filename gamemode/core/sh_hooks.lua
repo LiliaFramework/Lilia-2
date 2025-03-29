736d022da96cf7e2a9064f328bf50cd609bd4eb2
eon (bloodycop) <eon.bloodycop@gmail.com>
2025-03-29 16:06:22 +0200

    Update sh_hooks.lua

--- gamemode/core/sh_hooks.lua
+++ gamemode/core/sh_hooks.lua
@@ -44,7 +44,7 @@ local function DatabaseQuery()
                     if v.default ~= nil then colDef = colDef .. " DEFAULT '" .. tostring(v.default) .. "'" end
 
                     local alter = ("ALTER TABLE lia_characters ADD COLUMN %s"):format(colDef)
-                    lia.database.query(alter, function() MsgC(Color(83, 143, 239), "[Lilia] ", Color(0, 255, 0), "[Database] ", Color(255, 255, 255), string.format("Added missing column `%s`.\n", v.field)) end)
+                    lia.database.query(alter, function() lia.print("[Database] ", lia.color.green, string.format("Added missing column `%s`.\n", v.field)) end)
                 end
             end
         end)
