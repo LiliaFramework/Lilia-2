c12eb013632abc98afe9af139df0c189e03ab3a3
bloodycop <eon.bloodycop@gmail.com>
2025-04-03 20:35:26 +0300

    Create sh_faction.lua

---
+++ gamemode/core/libraries/sh_faction.lua
@@ -0,0 +1,19 @@
+lia.faction = lia.faction or {}
+lia.faction.stored = lia.faction.stored or {}
+lia.faction.indices = lia.faction.indices or {}
+
+function lia.faction.Setup( tFactionData )
+	local FACTION = {}
+
+	FACTION.name = tFactionData.name or "Faction Name"
+	FACTION.description = tFactionData.description or "Faction Description"
+	FACTION.color = tFactionData.color or lia.color.white
+	FACTION.isDefault = tFactionData.isDefault or true
+
+	FACTION.index = #lia.faction.indices + 1
+
+	lia.faction.stored[tFactionData.uniqueID] = FACTION
+	lia.faction.indices[FACTION.index] = FACTION
+
+	return FACTION.index
+end
