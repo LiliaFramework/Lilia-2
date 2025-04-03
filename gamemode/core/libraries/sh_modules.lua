6b47e1b11e4caa8d54091d86af664a0f961c9f8b
bloodycop <eon.bloodycop@gmail.com>
2025-04-03 19:13:44 +0300

    i forgot these

--- gamemode/core/libraries/sh_modules.lua
+++ gamemode/core/libraries/sh_modules.lua
@@ -1,14 +1,20 @@
 lia.module = lia.module or {}
 lia.module.stored = {}
 
+local modulePath = "lilia/gamemode/modules/"
+
 function lia.module.Load()
-	local modulePath = "lilia/gamemode/modules/"
 	local files, dirs = file.Find( modulePath .. "*", "LUA" )
 	for k, v in ipairs( dirs ) do
 		if hook.Run( "ModuleShouldLoad", v ) == false then continue end
 
 		if file.Exists( modulePath .. v .. "/sh_init.lua", "LUA" ) then
-			MODULE = { uniqueID = v }
+			MODULE = {}
+				MODULE.uniqueID = v
+				MODULE.name = MODULE.name or "Unknown Name"
+				MODULE.desc = MODULE.desc or "Unknown Description"
+				MODULE.author = MODULE.author or "Unknown Author"
+
 				lia.Include( modulePath .. v .. "/sh_init.lua" )
 
 				for k2, v2 in pairs(MODULE) do
--- gamemode/modules/thirdprson/cl_print.lua
+++ gamemode/modules/thirdprson/cl_print.lua
@@ -1,5 +1,4 @@
 local MODULE = MODULE
 
 function MODULE:CalcView(ply, origin, angles, fov) -- TODO, hooks ain't called, someone fix this shit
-	lia.print("CalcView", ply, origin, angles, fov)
 end
