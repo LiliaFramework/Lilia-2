e39012a2c9fb11186ae18b8a841fc9e71136e4cd
bloodycop <eon.bloodycop@gmail.com>
2025-04-06 21:48:11 +0300

    Update sh_item.lua

--- gamemode/core/libraries/sh_item.lua
+++ gamemode/core/libraries/sh_item.lua
@@ -1,6 +1,7 @@
 lia.item = lia.item or {}
 lia.item.stored = {}
 lia.item.indices = {}
+lia.item.bases = {}
 
 local itemDataDefaults = {
     name = "Unknown Item",
@@ -9,14 +10,29 @@ local itemDataDefaults = {
     model = "models/props_c17/oildrum001.mdl",
 }
 
-function lia.item.Register( tItemData )
+function lia.item.Register( tItemData, baseID, bIsBaseItem )
+    if tItemData.base and lia.item.bases[tItemData.base] then
+        tItemData = table.Copy(lia.item.bases[tItemData.base]) -- TODO: not sure if this is correct
+    end
+
     for k, v in pairs(itemDataDefaults) do
         if tItemData[k] == nil then
             tItemData[k] = v
         end
     end
 
-    lia.item.stored[tItemData.uniqueID] = tItemData
+    if tItemData.uniqueID == nil or not isstring(tItemData.uniqueID) then
+        lia.error("Item must have a uniqueID string.")
+        return
+    end
 
+    if lia.item.stored[tItemData.uniqueID] then
+        lia.error("Item with uniqueID '" .. tItemData.uniqueID .. "' already exists.")
+        return
+    end
+
+    (bIsBaseItem and lia.item.bases or lia.item.stored)[tItemData.uniqueID] = tItemData
+
+    hook.Run("OnItemRegistered", tItemData)
     return tItemData
 end
